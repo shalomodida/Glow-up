41a08d0c8504324ec52069de0b5b8d9e28a5c130
AkinyiOwuor <owuorrachel@gmail.com>
2020-03-04 16:10:36 +0300

    add js function to the slide

--- js/script.js
+++ js/script.js
@@ -14,17 +14,17 @@ $(document).ready(function(){
             alert(" Thank you " + name + " we have received your details.")
        }
     })
-//     var myIndex = 0;
-//           carousel();
-//           function carousel() {
-//             var i;
-//             var x = document.getElementsByClassName("mySlides");
-//             for (i = 0; i < x.length; i++) {
-//               x[i].style.display = "none";
-//             }
-//             myIndex++;
-//             if (myIndex > x.length) { myIndex = 1 }
-//             x[myIndex - 1].style.display = "block";
-//             setTimeout(carousel, 3000);
-//           }
-// })
+     var myIndex = 0;
+          carousel();
+          function carousel() {
+           var i;
+            var x = document.getElementsByClassName("mySlides");
+             for (i = 0; i < x.length; i++) {
+               x[i].style.display = "none";
+             }
+             myIndex++;
+             if (myIndex > x.length) { myIndex = 1 }
+             x[myIndex - 1].style.display = "block";
+             setTimeout(carousel, 3000);
+           }
+ })
