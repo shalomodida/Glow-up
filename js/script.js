cdfd7de22450cf3b6d1aa79409836ff15be8510b
Salem Owino <salemowino18@gmail.com>
2020-03-04 14:39:25 +0300

    latest designs

--- js/script.js
+++ js/script.js
@@ -1,53 +1,11 @@
-
-        
-// var modal = document.getElementById('id01');
-
-
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-$(document).ready(function(){
-  $("#sign-up").on('click', function() {
-      event.preventDefault();
-      var email=$("#email").val();
-      var password=$("#password").val();
-      var reppassword=$("#reppassword").val();
+$(document).ready(function () {
+  $(".container img").click(function () {
+    $(this).hide();
+  });
 
 
-      if(email ==''||password ==''||reppassword ==''){
-         alert("Please ensure you fill in all the fields!");
-     }
-      else{
-          alert(" Thank you. "  + " You have succesfully been signed-up for Glow-up collection,stay with us for unique designs.")
-     }
-  })
-  function finder(){
-    var searchedWord = document.getElementById('searchedWord').value;
-    if(searchedWord=="about"){
-       var mySearchResult = window.open("about.html");
-
-    //  if else(searchedWord==""){
-    //     var mySearchResult = window.open("ready.html");
-        
-    }else{
-       alert("Genre not listed");
-    }
- }
- function prompt(){
-    var searchedWord = document.getElementById('promote').value;
-       var mySearchResult = window.open("about.html");
-       }
-})
-
-
-
-
-
-
-
-
-
-
-
+  $(".eve1").hover(function () {
+    $(".para1").slideToggle(1000);
+    $("#img1").slideToggle(1000);
+    $(".para1").show();
+  });
