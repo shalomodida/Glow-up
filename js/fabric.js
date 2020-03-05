d5510ec13352448cb503a98b4733550da3183109
brian-475 <brianadala15@gmail.com>
2020-03-05 16:31:55 +0300

    pushing my work to github

--- js/fabric.js
+++ js/fabric.js
@@ -1,79 +1,73 @@
 $("document").ready(function(){
     $("#alt").click(function(){
         alert(" You have Choosen Battenberg lace and a meter is 1,500 ksh");
-        alert("Kindly add more to the Cart!!")
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt2").click(function(){
         alert("You have Choosen Bedford and a meter is 1,400ksh");
-        alert("Kindly add more to the Cart!!");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt3").click(function(){
         alert("You have Choosen Bengaline Silk and a meter is 1,500ksh");
-        alert("Kindly add more to the Cart!!");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt4").click(function(){
         alert("You have Choosen Beta Cloth and a meter is 1,500ksh ");
-        alert("Kindly add more to the Cart!!");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt5").click(function(){
         alert("You have Choosen Bobbinet and a meter is 1,500ksh");
-        alert("Kindly add more to the Cart!!");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt6").click(function(){
         alert("You have Choosen Boiled Wool and a meter is 1,500ksh ");
-        alert("Kindly add more to the Cart")
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt7").click(function(){
         alert("You have Choosen Bomabazine and a meter is 1,500ksh");
-        alert("Kindly add more to the Cart");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt8").click(function(){
         alert("You have Choosen Boucle and a meter is 1,500ksh");
-        alert("Kindly add more to the Cart");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt9").click(function(){
         alert("You have Choosen Barathea and a meter is 1,500ksh");
-        alert("Kindly add more to the Cart");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt10").click(function(){
         alert("You have Choosen Bark Cloth and a meter is 1,600ksh");
-        alert("Kindly add more to the Cart");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt11").click(function(){
         alert("You have Choosen Batik and a meter is 1,700ksh");
-        alert("Kindly add more to the Cart");
+        alert("Your Choice has been added to the Cart!!");
     })
     $("#alt12").click(function(){
         alert("You have Choosen Batiste and a meter is 1,900ksh");
-        alert("Kindly add more to the Cart");
+        alert("Your Choice has been added to the Cart!!");
     })
 })
     //my javaScript//
     $("document").ready(function(){
-        $("#home").click(function(){
-            var a=$("#alt").val();
-            var b=$("#alt2").val();
-            var c=$("#alt3").val();
-            var d=$("#alt4").val();
-            var e=$("#alt5").val();
-            var f=$("#alt6").val();
-            var g=$("#alt7").val();
-            var h=$("#alt8").val();
-            var i=$("#alt9").val();
-            var j=$("#alt10").val();
-            var k=$("#alt11").val();
-            var l=$("#alt12").val();
-            var total=(a+b+c+d+e+f+g+h+i+j+k+l);
-    
+        $("#button").click(function(){
+           
+            var coth=$("#cloth[name=value]:selected").val();
             
-                    if(a==="1300"){
-                        alert("You have Chosen Batenberg Lace and the amount is 1300");
+            var total=$()
+                    if(coth==="true"){
+                        alert("You have not Chosen any Amount!!");
                     }
-                    else if(b==="1400"){
-                          alert("You have Chosen Bedford and your total amount is 1,300Ksh")
-                          alert("Thank you for shopping with us")
+                    else {
+                        alert("total amount is 1600ksh!!")
+                        alert("we have received the information");
+                        prompt("would you love another design?");
+                        alert("Were happy to have been of help")
+                        prompt("Do you live within Kenya?");
+                        alert("Wait for our call");
                     }
                 });
+                event.preventDefault();
             });
            
         
