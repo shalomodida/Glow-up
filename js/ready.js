e5a507b1b98e2bd97c0547d877adc8ab424ec4d5
Samwelrangili <samwelrangili896@gmail.com>
2020-03-05 16:11:54 +0300

    pushing work done in all files to github repo

---
+++ js/ready.js
@@ -0,0 +1,67 @@
+
+$("document").ready(function(){
+    $("#one").click(function(){
+        alert(" You selected Dress and the price is Ksh.899 tax-free");
+        alert("Your selection has been added to the cart")
+    })
+    $("#two").click(function(){
+        alert("You selected Jeans and the price is Ksh.1200 tax-free");
+        alert("Your selection has been added to the cart");
+    })
+    $("#three").click(function(){
+        alert("You've selected Longeries and the price is Ksh.559 tax-free")
+        alert("Your selection has been added to the cart.")
+    })
+    $("#four").click(function(){
+        alert("You've selected Soft-coat and the price is Ksh.2699 tax-free")
+        alert("Your selection has been added to the cart.")
+    })
+    $("#five").click(function(){
+        alert("You've selected Poover and the price is Ksh.999 tax-free")
+        alert("Your selection has been added to the cart.")
+    })
+    $("#six").click(function(){
+        alert("You've selected Set-Suit and the price is Ksh.3599 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#seven").click(function(){
+        alert("You've selected Off-Sholder and the price is Ksh.790 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#eight").click(function(){
+        alert("You've selected Coat and the price is Ksh.1199 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#nine").click(function(){
+        alert("You've selected Kitty-Skirt and the price is Ksh.1999 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#ten").click(function(){
+        alert("You've selected Casual-Suit and the price is Ksh.3599 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#eleven").click(function(){
+        alert("You've selected Top and the price is Ksh.1989 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#twelve").click(function(){
+        alert("You've selected Track and the price is Ksh.2399 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#thirteen").click(function(){
+        alert("You've selected Trenchcoat and the price is Ksh.4500 tax-free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#forteen").click(function(){
+        alert("You've selected Material and the price is Ksh.5999 tax-free")
+        alert("Your selection has been added to the cart")
+    })  
+    $("#fiveteen").click(function(){
+        alert("You've selected Yellow-Suit and the price is Ksh.4999 tax free")
+        alert("Your selection has been added to the cart")
+    })
+    $("#sixteen").click(function(){
+        alert("You've selected Fendi Jacket and the price is Ksh.2500 tax free")
+        alert("Your selection has been added to the cart")
+    })
+})
