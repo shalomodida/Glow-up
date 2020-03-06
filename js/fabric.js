3a04a1f415ff4260ea53d6650c61b96ae2d5e593
brian-475 <brianadala15@gmail.com>
2020-03-06 09:15:41 +0300

    pushing current changes

--- js/fabric.js
+++ js/fabric.js
@@ -1,38 +1,38 @@
 $("document").ready(function(){
     $("#alt").click(function(){
-        alert(" You have Choosen Battenberg lace and a meter is 1,500 ksh");
+        alert(" You have Choosen Battenberg lace and a meter is 1,600 ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt2").click(function(){
-        alert("You have Choosen Bedford and a meter is 1,400ksh");
+        alert("You have Choosen Bedford and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt3").click(function(){
-        alert("You have Choosen Bengaline Silk and a meter is 1,500ksh");
+        alert("You have Choosen Bengaline Silk and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt4").click(function(){
-        alert("You have Choosen Beta Cloth and a meter is 1,500ksh ");
+        alert("You have Choosen Beta Cloth and a meter is 1,600ksh ");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt5").click(function(){
-        alert("You have Choosen Bobbinet and a meter is 1,500ksh");
+        alert("You have Choosen Bobbinet and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt6").click(function(){
-        alert("You have Choosen Boiled Wool and a meter is 1,500ksh ");
+        alert("You have Choosen Boiled Wool and a meter is 1,600ksh ");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt7").click(function(){
-        alert("You have Choosen Bomabazine and a meter is 1,500ksh");
+        alert("You have Choosen Bomabazine and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt8").click(function(){
-        alert("You have Choosen Boucle and a meter is 1,500ksh");
+        alert("You have Choosen Boucle and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt9").click(function(){
-        alert("You have Choosen Barathea and a meter is 1,500ksh");
+        alert("You have Choosen Barathea and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt10").click(function(){
@@ -40,11 +40,11 @@ $("document").ready(function(){
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt11").click(function(){
-        alert("You have Choosen Batik and a meter is 1,700ksh");
+        alert("You have Choosen Batik and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
     $("#alt12").click(function(){
-        alert("You have Choosen Batiste and a meter is 1,900ksh");
+        alert("You have Choosen Batiste and a meter is 1,600ksh");
         alert("Your Choice has been added to the Cart!!");
     })
 })
